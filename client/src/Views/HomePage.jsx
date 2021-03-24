965461839291dbf92cf118616a401ed9a8e000fd
LTurza <lukaszturza@gmail.com>
2021-03-24 22:18:52 +0100

    homePage.jsx & HomePage.scss

---
+++ client/src/Views/HomePage.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './../styles/HomePage.scss'
+
+class HomePage extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="home-page">
+                <div className="home-page__container first">asdasd</div>
+            </div>
+        )
+    }
+}
+
+export default HomePage
