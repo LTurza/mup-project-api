07ae960a46d667fc418309a9e0bfcc4ecfa10b73
LTurza <lukaszturza@gmail.com>
2021-03-29 22:55:26 +0200

    first-validation-schemas

--- server/controllers/teamController.js
+++ server/controllers/teamController.js
@@ -1,41 +1,42 @@
 const Team = require('./../models/team')
 const User = require('./../models/user')
 const ObjectId = require('mongodb').ObjectId
+const Ajv = require('ajv')
+
+const ajv = new Ajv()
+const { newTeamDataSchema, addMemberToTeamSchema } = require('./../validation/schemas.js')
 
 exports.postNewTeam = async (req,res) => {
-  const teamTitle = req.body.teamTitle
   const teamDescription = req.body.teamDescription
   const teamLogo = req.body.teamLogo
   const userId = req.body.userId
 
-  if (typeof teamTitle === 'string' && teamTitle.trimEnd().length > 0
-  && typeof teamDescription === 'string' && teamDescription.trimEnd().length > 0
-  && typeof teamLogo === 'string'&& teamLogo.trimEnd().length > 0 
-  && typeof userId === 'string' && userId.trimEnd().length === 24){
+  const validate = ajv.compile(newTeamDataSchema)
+  const valid = validate(req.body)
 
-      const teamExist = await Team.fetchTeamByData('title', teamTitle)
-      if (teamExist === null) {
-        
-        const user = await User.fetchUserByData('_id', ObjectId(userId))
-        const dataForNewTeam = {
-          admin: {
-            userId: ObjectId(user._id),
-            firstname: user.firstName,
-            secondName: user.lastName
-          },
-          title: teamTitle,
-          description: teamDescription,
-          logo: teamLogo,
-          members: [ObjectId(user._id)],
-          tasks: [] 
-        }
-  
-        const newTeam = new Team(dataForNewTeam)
-        await newTeam.addNewTeam()
-        res.status(201).json({message: 'Team created succesfully'})
-  
-      } else {
-        res.status(409).json({message: 'Team already exists.'})
+  if (valid){
+    const teamExist = await Team.fetchTeamByData('title', teamTitle)
+    if (teamExist === null) {
+      
+      const user = await User.fetchUserByData('_id', ObjectId(userId))
+      const dataForNewTeam = {
+        admin: {
+          userId: ObjectId(user._id),
+          firstname: user.firstName,
+          secondName: user.lastName
+        },
+        title: teamTitle,
+        description: teamDescription,
+        logo: teamLogo,
+        members: [ObjectId(user._id)],
+        tasks: [] 
+      }
+
+      const newTeam = new Team(dataForNewTeam)
+      await newTeam.addNewTeam()
+      res.status(201).json({message: 'Team created succesfully'})
+    } else {
+      res.status(409).json({message: 'Team already exists.'})
     }
   } else {
     res.status(400).json({msg: 'Invalid data'})
@@ -51,9 +52,10 @@ exports.getAllTeams = async (req, res) => {
 exports.putNewTeamMember = async (req, res) => {
   const userId = req.params.userId
   const teamId = req.params.teamId
+  const validate = ajv.compile(addMemberToTeamSchema)
+  const valid = validate(req.params)
 
-  if(typeof userId === 'string'&& userId.trim().length === 24 
-  && typeof teamId === 'string' && teamId.trim().length === 24) {
+  if(valid) {
 
     const isTeamExist = await Team.isTeamExist(teamId)
     const isUserExist = await User.isUserExisit(userId)
